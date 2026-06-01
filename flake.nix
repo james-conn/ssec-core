c8c252a2a909afcda0b17c9f9b107a5d8f3b967c
James Connolly <me@j-conn.com>
2026-05-31 19:26:31 -0700

    flake: nixpkgs 25.11 -> 26.05 (#11)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+		nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
 
